2def7ea1cf9c999f1c664d228ab8b3ebc9e15aa5
Silasoli <silas.oliveira@blockhub.builders>
2022-07-30 13:29:54 -0300

    Utilindo YMl pra configurar

--- Introdução ao framework Spring Boot/springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
+++ Introdução ao framework Spring Boot/springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/AppController.java
@@ -12,15 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author silas
  */
-
 @RestController
 public class AppController {
-    
+
     @Value("${app.message}")
     private String appMessage;
-    
+
+    @Value("${ENV_DB_URL:NENHUMA}")
+    private String dbEnvironmentVariable;
+
     @GetMapping("/")
-    public String getAppMessage(){
+    public String getAppMessage() {
         return appMessage;
+    }
+
+    @GetMapping("/envVariable")
+    public String getEnvironmentVariable() {
+        return "A seguinte variável de ambiente foi passada: " + dbEnvironmentVariable;
     }
 }
