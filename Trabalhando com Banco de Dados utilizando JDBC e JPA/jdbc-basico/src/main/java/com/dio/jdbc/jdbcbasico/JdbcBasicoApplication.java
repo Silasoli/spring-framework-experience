7e665b00f027721f5d62d53ad6eb813dbe9bf4b2
Silasoli <silas.oliveira@blockhub.builders>
2022-08-06 11:36:07 -0300

    Adicionando crud em java com banco de dados

--- Trabalhando com Banco de Dados utilizando JDBC e JPA/jdbc-basico/src/main/java/com/dio/jdbc/jdbcbasico/JdbcBasicoApplication.java
+++ Trabalhando com Banco de Dados utilizando JDBC e JPA/jdbc-basico/src/main/java/com/dio/jdbc/jdbcbasico/JdbcBasicoApplication.java
@@ -3,6 +3,7 @@ package com.dio.jdbc.jdbcbasico;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,39 +13,46 @@ public class JdbcBasicoApplication {
     public static void main(String[] args) throws SQLException {
         SpringApplication.run(JdbcBasicoApplication.class, args);
 
-        String URLConnection = "jdbc:mysql://localhost/digital_innovation_one";
+        AlunoDAO alunoDAO = new AlunoDAO();
 
-        try (Connection conn = DriverManager.getConnection(URLConnection, "root", "root")) {
-            System.out.println("Success");
-        } catch (Exception e) {
-            System.out.println("Failed");
-        };
+        // =========================== 1 - Consulta =================================================
+        List<Aluno> alunos = alunoDAO.list();
 
-//        // 1 - NÃO ESQUECER DE BAIXAR O DRIVER PARA O BANCO DE DADOS QUE IRÁ UTILIZAR! (Como demonstrado na parte 1 do curso)
-//        // 2 - Definir parâmetros para se conectar ao banco de dados MySQL.
-//        String driver = "mysql";
-//        String dataBaseAddress = "localhost";
-//        String dataBaseName = "digital_innovation_one";
-//        String user = "root";
-//        String password = "password";
-//
-//        // 3 - Construção da string de conexão.
-//        StringBuilder sb = new StringBuilder("jdbc:")
-//                .append(driver).append("://")
-//                .append(dataBaseAddress).append("/")
-//                .append(dataBaseName);
-//
-//        String connectionUrl = sb.toString(); //sb.toString() == "jdbc:mysql://localhost/digital_innovation_one"
-//
-//        //Carregar a classe específica de implementação do driver na memória da JVM. (A partir da versão JDBC 4 não é mais necessário!!!)
-//        //Class.forName("com.mysql.jdbc.Driver");
-//        // 4 - Criar conexão usando o DriverManager, passando como parâmetros a string de conexão, usuário e senha do usuário.
-//        try (Connection conn = DriverManager.getConnection(connectionUrl, user, password)) {
-//            System.out.println("SUCESSO ao se conectar ao banco MySQL!");
-//        } catch (SQLException e) {
-//            System.out.println("FALHA ao se conectar ao banco MySQL!");
-//            e.printStackTrace();
-//        }
+        System.out.println("Consulta");
+
+        alunos.stream().forEach(System.out::println);
+
+        // ======================= 1.1 - Consulta com filtro ========================================
+        Aluno alunoParaConsulta = alunoDAO.getById(3);
+
+        System.out.println("Consulta com filtro");
+
+        System.out.println(alunoParaConsulta);
+
+        // =========================== 2 - Inserção =================================================
+        System.out.println("Inserção");
+
+        Aluno alunoParaInsercao = new Aluno(
+                "Matheus",
+                43,
+                "SP"
+        );
+
+        alunoDAO.create(alunoParaInsercao);
+
+        // =========================== 3 - Delete ===================================================
+        System.out.println("Delete");
+
+        alunoDAO.delete(5);
+        // =========================== 4 - Atualizar ================================================
+        System.out.println("Atualizar");
+
+        Aluno alunoParaAtualizar = alunoDAO.getById(1);
+        alunoParaAtualizar.setNome("Joaquim");
+        alunoParaAtualizar.setIdade(18);
+        alunoParaAtualizar.setEstado("RS");
+
+        alunoDAO.update(alunoParaAtualizar);
     }
 
 }
