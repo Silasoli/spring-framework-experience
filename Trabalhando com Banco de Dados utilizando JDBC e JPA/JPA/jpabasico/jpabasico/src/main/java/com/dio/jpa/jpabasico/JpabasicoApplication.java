b43c03cb165166560f39fc611351647e942e9d49
Silasoli <silas.oliveira@blockhub.builders>
2022-08-07 10:51:02 -0300

    Adicionando JDBC e JPA project

---
+++ Trabalhando com Banco de Dados utilizando JDBC e JPA/JPA/jpabasico/jpabasico/src/main/java/com/dio/jpa/jpabasico/JpabasicoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.jpa.jpabasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpabasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpabasicoApplication.class, args);
+	}
+
+}
---
+++ Trabalhando com Banco de Dados utilizando JDBC e JPA/JPA/jpabasico/jpabasico/src/test/java/com/dio/jpa/jpabasico/JpabasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.jpa.jpabasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpabasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
