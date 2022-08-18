378afd472b1c0e1f564bcb9d913e193cacfae734
Silasoli <silas.oliveira@blockhub.builders>
2022-08-18 08:26:52 -0300

    Adicionando auth com JWT e H2

---
+++ Adicionando Segurança a uma API REST com Spring Security/dio-spring-security-jwt/src/main/java/diospringsecurityjwt/dtos/Sessao.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package diospringsecurityjwt.dtos;
+
+/**
+ *
+ * @author silas
+ */
+public class Sessao {
+
+    private String login;
+    private String token;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
---
+++ Adicionando Segurança a uma API REST com Spring Security/dio-spring-security-jwt/src/main/java/diospringsecurityjwt/service/UserService.java
@@ -0,0 +1,24 @@
+package diospringsecurityjwt.service;
+
+import diospringsecurityjwt.model.User;
+import diospringsecurityjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired()
+    private UserRepository repository;
+
+    @Autowired(required = false)
+    private PasswordEncoder encoder;
+
+    public void createUser(User user) {
+        String pass = user.getPassword();
+        //criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
